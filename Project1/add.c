f2d5037e9ebd043b490aed7e5f268d6678953ffc
MaroBytes <MaroBytes@aliyun.com>
2022-03-03 23:13:01 +0800

    🎈课堂练习代码提交

--- Project1/add.c
+++ Project1/add.c
@@ -1,15 +1,135 @@
 /*全局变量
 static int g_val = 2022;
-static定义的全局变量只能被自己所在的源文件使用
+//static定义的全局变量只能被自己所在的源文件使用
 */
 
-/*int Add(int x, int y)
+/*
+int Add(int x, int y)
 {
 	return x + y;
-}*/
+}
+*/
 
-//static修饰函数也只能被自己所在的源文件使用
+/*static修饰函数也只能被自己所在的源文件使用
 static int Add(int x, int y)
 {
 	return x + y;
 }
+*/
+
+/*
+extern int Add(int, int);
+int main()
+{
+	int a = 10, b = 20;
+	int sum = Add(a, b);
+	printf("sum = %d\n", sum);
+	return 0;
+}
+*/
+
+//3rd Mar,2022 cut main.c
+
+/*
+int main()
+{
+	int a = 10;
+	printf("%p\n", &a);
+	int* pa = &a;//pa是用来存放地址的，在C语言中pa称为指针变量
+	//* 说明pa是指针
+	//int 说明pa执行的对象是int类型的
+
+	char ch = 'w';
+	char* pc = &ch;
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	int a = 10;
+	int* pa = &a;
+	*pa = 20; //* 解引用操作符 *pa就是通过pa里面的地址找到a
+	printf("%d\n", a);
+	return 0;
+}
+*/
+
+/*
+struct Stu
+{
+	char name[20];
+	int age;
+	double score;
+};
+
+struct Book
+{
+	char name[20];
+	float price;
+	char id[20];
+};
+int main() {
+	struct Stu s = { "张三", 20, 85.5 };//结构体的创建和初始化
+	printf("1:%s %d %lf\n", s.name, s.age, s.score);//结构体变量.成员变量
+	struct Stu* ps = &s;
+	printf("2:%s %d %lf\n", (*ps).name, (*ps).age, (*ps).score);
+	printf("3:%s %d %lf\n", ps->name, ps->age, ps->score);
+	return 0;
+}
+*/
+
+/*VS2019\2022不支持C99的变长数组
+int main()
+{
+	int a = [n];
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	int a = 0;
+	int b = 2;
+	if (a = 1)
+		if (b = 2)
+			printf("hehe\n");
+	else
+		printf("haha\n");
+	return 0;
+}
+*/
+/*
+int main()
+{
+	char ch;
+	while ((ch=getchar())!=EOF)
+	{
+		putchar(ch);
+	}
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	char password[20] = { 0 };
+	printf("Please Input Your Password:");
+	scanf("%s", password);
+	printf("Please Confirm Your Password(Y/N):>");
+	getchar();
+	int ch = getchar();//getchar读取了缓冲区中的回车'\n'
+	if (ch == 'Y')
+	{
+		printf("Success!\n");
+	}
+	else
+	{
+		printf("Faild!");
+	}
+	return 0;
+}
+*/
--- Project1/main.c
+++ Project1/main.c
@@ -1,10 +1,138 @@
 #include <stdio.h>
+/*
+int main() 
+{
+	int ch = 0;
+	while ((ch=getchar())!=EOF)
+	{
+		if (ch < '0' || ch>'9')
+			continue;
+		putchar(ch);
+	}
+	return 0;
 
-extern int Add(int, int);
+}
+*/
+
+/*二分查找简单示例
 int main()
 {
-	int a = 10, b = 20;
-	int sum = Add(a, b);
-	printf("sum = %d\n", sum);
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	//在arr这个有序数组中查找k的值
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	while(left<=right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			printf("找到了！>:%d", mid);
+			break;
+		}
+	}
+	if (left>right)
+	{
+		printf("找不到了！");
+	}
 	return 0;
 }
+*/
+
+/*
+#include <Windows.h>
+int main()
+{
+	char arr1[] = "Welcome to TGU!!!!!!";
+	char arr2[] = "####################";
+	int left = 0;
+	int right = sizeof(arr1) / sizeof(char) - 2;//此处求出包括'\0'在内的元素个数，-2算最后一个字符下标
+
+	while (left<=right)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		Sleep(1000);//休眠1000ms
+		system("cls");//清屏
+		left++;
+		right--;
+	}
+	printf("%s", arr2);
+	return 0;
+}
+*/
+
+/*猜数字游戏
+#include <stdlib.h>
+#include <time.h>
+void menu()
+{
+	printf("*********************\n");
+	printf("*******1.Play********\n");
+	printf("*******0.Exit********\n");
+	printf("*********************\n");
+}
+void game()
+{
+	//猜数字游戏的实现
+	//1.生成随机数
+	//rand函数生成
+
+	int ret = rand()%100 + 1;//0-100的随机数
+	int guess = 0;
+	//2.猜数字
+	while (1)
+	{
+		printf("请猜数字：>");
+		scanf("%d", &guess);
+		if (guess < ret)
+		{
+			printf("猜小了\n");
+		}
+		else if (guess > ret)
+		{
+			printf("猜大了\n");
+		}
+		else
+		{
+			printf("恭喜你！猜对了！\n");
+			break;
+		}
+	}
+
+}
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));//用时间戳作随机数种子
+	do
+	{
+		menu();
+		printf("请选择：>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新选择\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
+*/
